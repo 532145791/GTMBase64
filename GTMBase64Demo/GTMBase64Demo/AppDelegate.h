ba9df90af4a341de182f35b834c1d5c6c235ad1c
uen <uen@uendemac-mini.local>
2017-05-15 15:21:29 +0800

    gtmbase64 文件下载

---
+++ GTMBase64Demo/GTMBase64Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GTMBase64Demo
+//
+//  Created by uen on 2017/5/15.
+//  Copyright © 2017年 UEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GTMBase64Demo/GTMBase64Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GTMBase64Demo
+//
+//  Created by uen on 2017/5/15.
+//  Copyright © 2017年 UEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
